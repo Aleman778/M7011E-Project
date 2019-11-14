0a565bc0cc178f8e1c04dbf52ce1dfb74305741a
Blinningjr <nicke.l@telia.com>
2019-11-14 10:55:10 +0100

    Undid last commit

--- simulator/controller.js
+++ simulator/controller.js
@@ -10,7 +10,7 @@ var simulator = require('./model.js');
 exports.getWindSpeed = function(req, res) {
     res.setHeader('Content-Type', 'application/json');
     var date = new Date();
-    var wind_speed = simulator.wind.getWindSpeed(date.getHours());
+    let wind_speed = simulator.wind.getWindSpeed(date.getHours());
     res.end(JSON.stringify({ wind_speed: wind_speed }));
 }
 
