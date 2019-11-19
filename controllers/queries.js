e26b16303289f4d0e42060aad25306c588e65ed1
Blinningjr <nicke.l@telia.com>
2019-11-19 11:50:17 +0100

    added querie and som small impovements

--- controllers/queries.js
+++ controllers/queries.js
@@ -1,3 +1,12 @@
+/***************************************************************************
+ * The queries controller defines the different queries and connection to
+ * Postgresql database.
+ ***************************************************************************/
+
+
+/**
+ * Defines a pool with the connection information.
+ */
 const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'admin',
@@ -8,8 +17,11 @@ const pool = new Pool({
 })
 
 
-const getALlWindSpeed = (request, response) => {
-  console.log("getALlWindSpeed");
+/**
+ * Returns the whole windspeed history.
+ */
+const getAllWindSpeed = (request, response) => {
+  console.log("getAllWindSpeed");
     pool.query('SELECT * FROM wind ORDER BY measured ASC', (error, results) => {
         if (error) {
         throw error
@@ -18,7 +30,21 @@ const getALlWindSpeed = (request, response) => {
     })
 }
 
+/**
+ * Returns the whole windspeed history.
+ */
+const insertWindSpeed = (request, response) => {
+  console.log("insertWindSpeed");
+    pool.query("INSERT INTO wind VALUES ('" + req.params.timeStamp + "','" +  req.params.windSpeed  + "'); ", (error, results) => {
+        if (error) {
+        throw errors
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
 
 module.exports = {
   getALlWindSpeed,
+  insertWindSpeed,
 }
