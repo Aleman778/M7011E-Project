1f25c0b0ff33920504773b2a354ab2c7f3bbbb5d
Alexander Mennborg <alexander.mennborg@gmail.com>
2019-11-27 09:30:30 +0100

    Finished with the design of the login view, added public folder

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ var db = require('./routes/db.js');
 app = express();
 port = process.env.PORT || 3000;
 
+// Set static files folder
+app.use(express.static('public'));
+
 // Set ejs as the template engine
 app.set('view engine', 'ejs');
 
