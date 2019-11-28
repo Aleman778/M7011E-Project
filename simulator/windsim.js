2ce7b9cd080982a32c05e0771ec054732c5797cf
Niklas <nicke.l@telia.com>
2019-11-28 13:01:04 +0100

    Update wind sim (#11)
    * Change time to be reprecented by timestamp in db and date in sim
    
    * made so that the day change and year
    
    * added non working query

--- simulator/windsim.js
+++ simulator/windsim.js
@@ -3,6 +3,7 @@
  *  One example on how to model this is to first sample a Gaussian distribution in order to get the mean 
  *      value of the wind speed for a given day of the year. 
  *  Then use that mean value in another Gaussian distribution to sample the wind speeds during that day.
+ *  Note: Max is used instead of mean.
  */
 class WindSim {
     /**
@@ -12,59 +13,72 @@ class WindSim {
      * @param {*} unit is the unit for the wind speed.
      */
     constructor(max, standardDeviation, unit) {
+        this.time = new Date()
+        this.time.setDate(this.time.getDate() -5);
+
         this.max = max;
         this.standardDeviation = standardDeviation;
         this.unit = unit;
-        this.day = 0;
-        this.year = 0;
-        this.day = 0;
+
         this.db = require('./../controllers/queries.js');
-        this.daysMean = new Array(365);
-        var step = (standardDeviation * 3.0) / 365.0;
-        for (var i = 0; i < 365; i++) {
-            this.daysMean[i] = this.gaussianDist(step * (i - 182), max, 0, standardDeviation);
-        }
-        this.daysMean = this.shuffle(this.daysMean);
-        this.windSpeed = new Array(24);
-        this.calcNewDaysWindspeed();
+        this.calcNewYear();
+        this.calcNewDay();
     }
 
+    calcNewYear() {
+        var days = this.daysInYear(this.time.getFullYear());
+        this.daysMax = new Array(days);
+        var step = (this.standardDeviation * 3.0) / days;
+        for (var i = 0; i < days; i++) {
+            this.daysMax[i] = this.gaussianDist(step * (i - (days/2)), this.max, 0, this.standardDeviation);
+        }
+        this.daysMax = this.shuffle(this.daysMax);
+    }
 
     /**
      * Calculates the windspeeds for the new day.
      */
-    calcNewDaysWindspeed() {
+    calcNewDay() {
+        this.windSpeed = new Array(24);
         var step = (this.standardDeviation * 6.0) / 24.0;
         for (var i = 0; i < 24; i++) {
-          this.windSpeed[i] = this.gaussianDist(step * (i - 12), this.daysMean[this.day], 0, this.standardDeviation);
+          this.windSpeed[i] = this.gaussianDist(step * (i - 12), this.daysMax[this.time.getDate() + this.time.getMonth() * 31], 0, this.standardDeviation);
         }
         this.windSpeed = this.shuffle(this.windSpeed);
         for (var i = 0; i < 24; i++) {
-            this.db.insertWindSpeed(this.year, this.day, i, this.windSpeed[i], this.unit);
+            this.time.setHours(i);
+            this.db.insertWindSpeed(this.time.getTime()/1000, this.windSpeed[i], this.unit);
         }
     }
 
 
     /**
-     * Changes the current day to the next day.
+     * Updates the wind speed value and the date value.
      */
-    newDay() {
-        this.day += 1;
-        if (this.day >= 365) {
-            this.year += 1;
-            this.day = 0;
-            this.daysMean = this.shuffle(this.daysMean);
+    updateDate() {
+        var date = new Date();
+        var differenceInTime = date.getTime() - this.time.getTime(); 
+        if (differenceInTime > 0) {
+            var differenceInDays = differenceInTime / (1000 * 3600 * 24);
+            for (var i = 0; i < differenceInDays; i++) {
+                this.time.setDate(this.time.getDate() + 1);
+                if (this.getDay(this.time) == 0) {
+                    this.calcNewYear();
+                }
+                this.calcNewDay();
+            }
         }
-        this.calcNewDaysWindspeed();
     }
 
 
     /**
      * Retrives the wind speed at a given hour.
-     * @param {*} hour is the hour of the day (0 - 23).
+     * @param {*} date is the date of when the wind speed was measured.
      */
-    getWindSpeed(hour) {
-        return this.windSpeed[hour];
+    getWindSpeed(date) {
+        this.updateDate()
+        
+        return this.windSpeed[0];
     }
 
 
@@ -94,6 +108,23 @@ class WindSim {
             a[j] = x;
         }
         return a;
+    }
+
+    getDay(date) {
+        var start = new Date(date.getFullYear(), 0, 0);
+        var diff = (date - start) + ((start.getTimezoneOffset() - date.getTimezoneOffset()) * 60 * 1000);
+        var oneDay = 1000 * 60 * 60 * 24;
+        return Math.floor(diff / oneDay);
+    }
+
+    daysInYear(year) {
+        if(year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+            // Leap year
+            return 366;
+        } else {
+            // Not a leap year
+            return 365;
+        }
     }
 }
 
