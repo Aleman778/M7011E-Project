0eb0079d5cf994e740d5d6e4fc62116e774ec757
Alexander Mennborg <alexander.mennborg@gmail.com>
2019-11-28 13:29:35 +0100

    Restructured the project to separate simulator and prosumer/manager web-server (#13)

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Installing node.js ..."
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install nodejs
-echo "Installing node packages..."
-npm install
-echo "Starting server..."
-npm start
