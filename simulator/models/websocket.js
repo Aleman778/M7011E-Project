f5762dbb673d314c733c23e0b7878a12d35d3c54
Blinningjr <nicke.l@telia.com>
2019-12-03 16:52:20 +0100

    Added a webSocket that sends the data with freqency 0.1 Hz

---
+++ simulator/models/websocket.js
@@ -0,0 +1,33 @@
+/**
+ *  Define needed constants and global variable.
+ */
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8000 });
+
+var windSim;
+
+
+/**
+ *  Sets the windSim variable which is the WindSim used to output to the socket.
+ */
+exports.setWindSim = function(windS) {
+    windSim = windS;
+}
+
+
+/**
+ *  Handles the socket connection and sending the wind speed data.
+ */
+wss.on('connection', function connection(ws) {
+    var interval = setInterval(
+        async function() {
+            var obj = new Object();
+            obj.time = new Date();
+            obj.wind_speed = await windSim.getWindSpeed(obj.time);
+            ws.send(JSON.stringify(obj));
+        }, 100);
+    
+    ws.on('close', function closing(message) {
+        clearInterval(interval);
+    });
+});
