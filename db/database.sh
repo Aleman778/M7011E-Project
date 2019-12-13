2fb2a467398d558724c52c6d6b7fdbfc74f04835
Niklas <nicke.l@telia.com>
2019-12-13 10:24:47 +0100

    Prosumer/data table (#20)
    * removed env var for wind database name
    
    * Change the database.sh to make two databaese and a user for each
    
    * Added env vars for the new database users and integrated them

--- db/database.sh
+++ db/database.sh
@@ -1,22 +1,48 @@
 #!/bin/bash
 set -e
 
-POSTGRES="psql -d ${POSTGRES_DB} -a -U${POSTGRES_USER}"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
+    CREATE USER climate WITH PASSWORD '${CLIMATE_PASSWORD}';
+	CREATE DATABASE climate;
+    GRANT ALL PRIVILEGES ON DATABASE climate TO climate;
+    
+    \c climate;
 
-$POSTGRES <<EOSQL
-CREATE TABLE ${PG_TABLE_WIND}(
-   time TIMESTAMP NOT NULL PRIMARY KEY,
-   windSpeed REAL NOT NULL,
-   unit VARCHAR NOT NULL
-);
-CREATE TABLE users (
-    id UUID PRIMARY KEY,
-    name VARCHAR(128) NOT NULL,
-    email VARCHAR(128) UNIQUE NOT NULL,
-    password VARCHAR(128) NOT NULL,
-    role VARCHAR(20) NOT NULL,
-    removed BOOL NOT NULL,
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP
-);
+    CREATE TABLE wind_data (
+        time TIMESTAMP NOT NULL PRIMARY KEY,
+        wind_speed REAL NOT NULL,
+        unit VARCHAR NOT NULL
+    );
+
+    GRANT ALL PRIVILEGES ON TABLE wind_data TO climate;
+
+    
+    CREATE USER electricity_grid WITH PASSWORD '${ELECTRICITY_GRID_PASSWORD}';
+    CREATE DATABASE electricity_grid;
+    GRANT ALL PRIVILEGES ON DATABASE electricity_grid TO electricity_grid;
+
+    \c electricity_grid;
+
+    CREATE TABLE users (
+        id UUID PRIMARY KEY,
+        name VARCHAR(128) NOT NULL,
+        email VARCHAR(128) UNIQUE NOT NULL,
+        password VARCHAR(128) NOT NULL,
+        role VARCHAR(20) NOT NULL,
+        removed BOOL NOT NULL,
+        created_at TIMESTAMP,
+        updated_at TIMESTAMP
+    );
+    GRANT ALL PRIVILEGES ON TABLE users TO electricity_grid;
+
+    CREATE TABLE prosumer_data (
+        id UUID PRIMARY KEY REFERENCES users(id),
+        time TIMESTAMP NOT NULL,
+        production REAL NOT NULL,
+        consumption REAL NOT NULL,
+        buffer REAL NOT NULL,
+        buffer_max REAL NOT NULL,
+        buffer_storing_limit REAL NOT NULL
+    );
+    GRANT ALL PRIVILEGES ON TABLE prosumer_data TO electricity_grid;
 EOSQL
--- simulator/controllers/wind-data.js
+++ simulator/controllers/wind-data.js
@@ -9,10 +9,10 @@
  */
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: process.env.PG_USER,
+  user: 'climate',
   host: 'db',
-  database: process.env.PG_DB,
-  password: process.env.PG_PASSWORD,
+  database: 'climate',
+  password: process.env.CLIMATE_PASSWORD,
 })
 
 
@@ -20,8 +20,8 @@ const pool = new Pool({
  * Returns the whole windspeed history.
  */
 exports.getAllWindSpeed = function(req, res) {
-  console.log(`Get all wind speeds from${process.env.PG_TABLE_WIND}`);
-  pool.query(`SELECT * FROM ${process.env.PG_TABLE_WIND} ORDER BY time DESC`, (error, results) => {
+  console.log(`Get all wind speeds from wind_data`);
+  pool.query(`SELECT * FROM wind_data ORDER BY time DESC`, (error, results) => {
       if (error) {
       throw error
       }
@@ -34,8 +34,8 @@ exports.getAllWindSpeed = function(req, res) {
  * Returns the nearest wind speed that is lower or equal to timestamp.
  */
 exports.getWindSpeedLowEqual = function(req, res) {
-  console.log(`Get wind speed lower equal from ${process.env.PG_TABLE_WIND}`);
-  pool.query(`SELECT * FROM ${process.env.PG_TABLE_WIND} WHERE time = (SELECT max(time) FROM ${process.env.PG_TABLE_WIND} WHERE time <= to_timestamp($1));`, [req.params.timestamp], (error, results) => {
+  console.log(`Get wind speed lower equal from wind_data`);
+  pool.query(`SELECT * FROM wind_data WHERE time = (SELECT max(time) FROM wind_data WHERE time <= to_timestamp($1));`, [req.params.timestamp], (error, results) => {
       if (error) {
       throw error
       }
@@ -48,8 +48,8 @@ exports.getWindSpeedLowEqual = function(req, res) {
  * Returns the nearest wind speed that is higher or equal to timestamp.
  */
 exports.getWindSpeedHighEqual = function(req, res) {
-  console.log(`Get wind speed higher or equal from ${process.env.PG_TABLE_WIND}`);
-  pool.query(`SELECT * FROM ${process.env.PG_TABLE_WIND} WHERE time = (SELECT min(time) FROM ${process.env.PG_TABLE_WIND} WHERE time > to_timestamp($1));`, [req.params.timestamp], (error, results) => {
+  console.log(`Get wind speed higher or equal from wind_data`);
+  pool.query(`SELECT * FROM wind_data WHERE time = (SELECT min(time) FROM wind_data WHERE time > to_timestamp($1));`, [req.params.timestamp], (error, results) => {
       if (error) {
       throw error
       }
@@ -63,8 +63,8 @@ exports.getWindSpeedHighEqual = function(req, res) {
  * Returns the latest measured wind speed.
  */
 exports.getLatestWindSpeed = function(req, res) {
-  console.log(`Get latest wind speed from ${process.env.PG_TABLE_WIND}`);
-  pool.query(`SELECT * FROM ${process.env.PG_TABLE_WIND} WHERE time = (SELECT max(time) FROM ${process.env.PG_TABLE_WIND});`, (error, results) => {
+  console.log(`Get latest wind speed from wind_data`);
+  pool.query(`SELECT * FROM wind_data WHERE time = (SELECT max(time) FROM wind_data);`, (error, results) => {
       if (error) {
       throw error
       }
@@ -77,8 +77,8 @@ exports.getLatestWindSpeed = function(req, res) {
  * Returns the two nearest wind speeds to timestamp, ordered buy time.
  */
 exports.getNearestWindSpeeds = function(req, res) {
-  console.log(`Get nearest wind speed from ${process.env.PG_TABLE_WIND}`);
-  pool.query(`SELECT * FROM ${process.env.PG_TABLE_WIND} WHERE time = (SELECT max(time) FROM ${process.env.PG_TABLE_WIND} WHERE time <= to_timestamp($1)) UNION ALL SELECT * FROM ${process.env.PG_TABLE_WIND} WHERE time = (SELECT min(time) FROM ${process.env.PG_TABLE_WIND} WHERE time > to_timestamp($1));`, [req.params.timestamp], (error, results) => {
+  console.log(`Get nearest wind speed from wind_data`);
+  pool.query(`SELECT * FROM wind_data WHERE time = (SELECT max(time) FROM wind_data WHERE time <= to_timestamp($1)) UNION ALL SELECT * FROM wind_data WHERE time = (SELECT min(time) FROM wind_data WHERE time > to_timestamp($1));`, [req.params.timestamp], (error, results) => {
     if (error) {
     throw error
     }
