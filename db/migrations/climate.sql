70d5c7d2d9d055cf8c08d251311eb53287fff5d0
Alexander Mennborg <alexander.mennborg@gmail.com>
2019-12-21 12:31:56 +0100

    Added database migrations, added gravatar flag in the users table

---
+++ db/migrations/climate.sql
@@ -0,0 +1,5 @@
+CREATE TABLE wind_data (
+    time TIMESTAMP NOT NULL PRIMARY KEY,
+    wind_speed REAL NOT NULL,
+    unit VARCHAR NOT NULL
+);
---
+++ db/migrations/electricity_grid.sql
@@ -0,0 +1,20 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    name VARCHAR(128) NOT NULL,
+    email VARCHAR(128) UNIQUE NOT NULL,
+    password VARCHAR(128) NOT NULL,
+    role VARCHAR(20) NOT NULL,
+    gravatar BOOL NOT NULL,
+    removed BOOL NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
+CREATE TABLE prosumer_data (
+    id UUID PRIMARY KEY REFERENCES users(id),
+    time TIMESTAMP NOT NULL,
+    production REAL NOT NULL,
+    consumption REAL NOT NULL,
+    buffer REAL NOT NULL,
+    buffer_max REAL NOT NULL,
+    buffer_storing_limit REAL NOT NULL
+);
