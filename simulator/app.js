d4d037dbcfd51447aa2b7beaf6f3d8638c33850b
Alexander Mennborg <alexander.mennborg@gmail.com>
2020-01-02 17:47:32 +0100

    Working on rewriting the simulator to improve the overall design

--- simulator/app.js
+++ simulator/app.js
@@ -1,35 +1,33 @@
 
 /***************************************************************************
- * The main server application.
+ * The main server entry point.
  ***************************************************************************/
 
 
 var express = require('express');
-var simulator = require('./routes/simulator.js');
-var db = require('./routes/db.js');
+var Simulator = require('./simulator');
+var wind = require('./api/wind');
 app = express();
 port = process.env.PORT || 3000;
 
-/**
- *  Allows get requests from http://localhost:3100.
- */
+
+//Allows get requests from http://localhost:3100.
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3100');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-// Adds support for input from POST requests.
+// Adds support for input from POST requests
 app.use(express.urlencoded({extended: true}));
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
-// Setup the simulator routes
-app.use('/simulator', simulator);
-
-// Setup the database routes
-app.use('/db', db);
-
-
+// Start the server
 app.listen(port);
+
+// Start the simulator
+var simulator = new Simulator();
+simulator.restore();
+simulator.run(1000 * 10);
---
+++ simulator/routes/api/wind.js
@@ -0,0 +1,5 @@
+
+
+var express = require('express');
+var simulator = require('..')
+var router = express.Router();
