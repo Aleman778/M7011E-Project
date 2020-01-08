6ffd1b11546bbfa46ce19123e4e61d34c0eba64b
Blinningjr <nicke.l@telia.com>
2020-01-08 22:27:50 +0100

    Improved the design of the prosumer info page and did som other small changes

--- web-server/public/js/list-prosumers.js
+++ web-server/public/js/list-prosumers.js
@@ -14,6 +14,7 @@ for (var p in prosumers) {
     let cell4 = row.insertCell(4);
     let cell5 = row.insertCell(5);
     let cell6 = row.insertCell(6);
+    let cell7 = row.insertCell(7);
 
     cell0.innerHTML = true;
     cell1.innerHTML = prosumers[p].name;
@@ -21,14 +22,15 @@ for (var p in prosumers) {
     cell3.innerHTML = prosumers[p].removed;
     cell4.innerHTML = prosumers[p].created_at;
     cell5.innerHTML = false;
-    cell6.innerHTML = ''
+    cell6.innerHTML = false;
+    cell7.innerHTML = ''
         + '<div class="button-container">'
         +   '<form action="/manager/prosumer/info" method="POST">'
         +       '<button class="btn octicon octicon-info" type="submit" name="prosumerId" value="' + prosumers[p].id + '"></button>'
         +   '</form>'
         +   '|'
         +   '<form action="/manager/block/prosumer" method="POST">'
-        +       '<input type="number" name="timeout" min="10" max="100" size="3">'
+        +       '<input type="number" name="timeout" min="10" max="100" size="3" value="10">'
         +       '<button class="btn octicon octicon-circle-slash" type="submit" name="prosumerId" value="' + prosumers[p].id + '"></button>'
         +   '</form>'
         +   '|'
