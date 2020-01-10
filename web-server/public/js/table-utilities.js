055f78902a894492e499bf03289553a18a96b4f4
Blinningjr <nicke.l@telia.com>
2020-01-10 16:47:10 +0100

    Added a shearch bar for prosumer list

--- web-server/public/js/table-utilities.js
+++ web-server/public/js/table-utilities.js
@@ -1,3 +1,8 @@
+/******************************************************************************
+ * Script for adding dynamic functionality to table. 
+ ******************************************************************************/
+
+
 /**
  * Sorts a table.
  * Note: Needs var tableId.
@@ -58,3 +63,42 @@ function sortTable(n) {
         }
     }
 }
+
+
+/**
+ * Filters table.
+ * Note: Needs var inputFilterId.
+ * @param {array<number>} columns the columns that can be filtered.
+ */
+function filterTable(columns) {
+    // Declare variables
+    let input = document.getElementById(inputFilterId);
+    let filter = input.value.toUpperCase();
+    let table = document.getElementById(tableId);
+    let tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (var i = 0; i < tr.length; i++) {
+        let td = [];
+        for (var j = 0; j < columns.length; j++) {
+            td.push(tr[i].getElementsByTagName("td")[columns[j]]);
+        }
+
+        let show = false;
+        for (var j = 0; j < columns.length; j++) {
+            if (td[j]) {
+                let txtValue = td[j].textContent || td[j].innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    show = true;
+                    break;
+                }
+            }
+        }
+
+        if (show) {
+            tr[i].style.display = "";
+        } else {
+            tr[i].style.display = "none";
+        }
+    }
+  }
