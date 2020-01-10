5e35e557eb24b66daf35a37deb2afc867e27ee5d
Blinningjr <nicke.l@telia.com>
2020-01-10 17:51:48 +0100

    Added so that removed prosumers don't display anything on column online, blocked and blackOut

--- web-server/public/js/list-prosumers-update.js
+++ web-server/public/js/list-prosumers-update.js
@@ -15,13 +15,19 @@ window.onload = function() {
 
         for (let i in prosumers) {
             let p = prosumers[i];
-            document.getElementById(p.id + ".online").innerHTML = p.online;
             document.getElementById(p.id + ".name").innerHTML = p.name;
             document.getElementById(p.id + ".email").innerHTML = p.email;
             document.getElementById(p.id + ".removed").innerHTML = p.removed;
             document.getElementById(p.id + ".created_at").innerHTML = p.created_at;
-            document.getElementById(p.id + ".blocked").innerHTML = p.blocked;
-            document.getElementById(p.id + ".blackOut").innerHTML = p.blackOut;
+            if (p.removed) {
+                document.getElementById(p.id + ".online").innerHTML = "";
+                document.getElementById(p.id + ".blocked").innerHTML = "";
+                document.getElementById(p.id + ".blackOut").innerHTML = "";
+            } else {
+                document.getElementById(p.id + ".online").innerHTML = p.online;
+                document.getElementById(p.id + ".blocked").innerHTML = p.blocked;
+                document.getElementById(p.id + ".blackOut").innerHTML = p.blackOut;
+            }
         }
         
     }, 5000);
